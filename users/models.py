35c6ac05f8832036d9a8e51d01fa938a7c190929
glebbolgarov <glebbolgarov@gmail.com>
2023-12-19 17:48:10 +0300

    feat: added user mode

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, auto_created=True)
+    def __str__(self) -> str:
+        return self.username
